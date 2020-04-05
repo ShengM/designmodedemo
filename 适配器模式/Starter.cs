7c7d5ab8ef499ae7e40f164a02b5d7369d16e81f
masheng <sheng7m@gmail.com>
2020-04-06 00:31:17 +0800

    添加适配器模式示例代码

---
+++ 适配器模式/Starter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace designmodedemo.适配器模式
+{
+  interface IEncrypt
+  {
+    String Encrypt(String originalInfomation);
+  }
+
+  class Encrypt
+  {
+    public String Reverse(String msg)
+    {
+      var msgChars = msg.ToCharArray();
+      Array.Reverse(msgChars);
+      return String.Join("", msgChars);
+    }
+  }
+
+  class EncryptAdapter : IEncrypt
+  {
+    private Encrypt _encryptor = new Encrypt();
+
+    public string Encrypt(string originalInfomation)
+    {
+      return _encryptor.Reverse(originalInfomation);
+    }
+  }
+
+  class Starter : IStart
+  {
+    public void Run()
+    {
+      var encryptor = new EncryptAdapter();
+      var encryptedMsg = encryptor.Encrypt("name:Jim,mail:jim@163.com");
+      Console.WriteLine(encryptedMsg);
+    }
+  }
+}
