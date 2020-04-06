e0b7175995d442c7f82b2731ad379198ffcedfa1
masheng <sheng7m@gmail.com>
2020-04-06 19:16:42 +0800

    添加桥接模式示例代码

---
+++ 桥接模式/Starter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace designmodedemo.桥接模式
+{
+  abstract class DataConverterBase
+  {
+    public IDatabaseReader DatabaseReader { get; set; }
+
+    public abstract void Convert();
+  }
+
+  class TxtConverter : DataConverterBase
+  {
+    public override void Convert()
+    {
+      this.DatabaseReader.Read();
+      Console.WriteLine("转换为TXT");
+    }
+  }
+
+  class PDFConverter : DataConverterBase
+  {
+    public override void Convert()
+    {
+      this.DatabaseReader.Read();
+      Console.WriteLine("转换为PDF");
+    }
+  }
+
+  class XMLConverter : DataConverterBase
+  {
+    public override void Convert()
+    {
+      this.DatabaseReader.Read();
+      Console.WriteLine("转换为XML");
+    }
+  }
+
+  public class MySQLReader : IDatabaseReader
+  {
+    public void Read()
+    {
+      Console.WriteLine("读取MySQL");
+    }
+  }
+
+  public class AccessReader : IDatabaseReader
+  {
+    public void Read()
+    {
+      Console.WriteLine("读取Access");
+    }
+  }
+
+  interface IDatabaseReader
+  {
+    void Read();
+  }
+
+  class Starter : IStart
+  {
+    public void Run()
+    {
+      var reader = new AccessReader();
+      DataConverterBase converter = new TxtConverter();
+      converter.DatabaseReader = reader;
+      converter.Convert();
+
+      converter = new PDFConverter();
+      converter.DatabaseReader = reader;
+      converter.Convert();
+    }
+  }
+}
