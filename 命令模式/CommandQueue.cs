fcfc39d36201969411d6ad43524b1431d61b2ca2
masheng <sheng7m@gmail.com>
2020-04-11 22:43:16 +0800

    添加命令模式示例代码

---
+++ 命令模式/CommandQueue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace designmodedemo.命令模式
+{
+  public abstract class Command
+  {
+    public abstract void Execute(int value);
+    public abstract void Undo();
+  }
+
+  class CommandQueue
+  {
+    private List<Command> _commands = new List<Command>();
+
+    public void AddCommand(Command command)
+    {
+      _commands.Add(command);
+    }
+
+    public void RemoveCommand(Command command)
+    {
+      _commands.Remove(command);
+    }
+
+    public void Execute(int value)
+    {
+      _commands.ForEach(command => command.Execute(value));
+    }
+  }
+
+  class Invoker
+  {
+    private CommandQueue _commandQueue;
+
+    public Invoker(CommandQueue commandQueue)
+    {
+      _commandQueue = commandQueue;
+    }
+
+    public void SetCommandQueue(CommandQueue commandQueue)
+    {
+      _commandQueue = commandQueue;
+    }
+
+    public void Call(int value)
+    {
+      _commandQueue.Execute(value);
+    }
+  }
+}
---
+++ 命令模式/Starter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace designmodedemo.命令模式
+{
+public class AddCommand : Command
+{
+  private readonly Adder _adder = new Adder();
+  private int _lastValue = 0;
+
+  public override void Execute(int value)
+  {
+    var result = _adder.Add(value);
+    _lastValue = value;
+    Console.WriteLine("Add Result: {0}", result);
+  }
+
+  public override void Undo()
+  {
+    var result = _adder.Add(-_lastValue);
+    Console.WriteLine("Undo Result: {0}", result);
+  }
+}
+
+public class Adder
+{
+  private int _sum = 0;
+
+  public int Add(int num)
+  {
+    _sum += num;
+    return _sum;
+  }
+}
+
+class Starter : IStart
+{
+  public void Run()
+  {
+    var c = new AddCommand();
+    c.Execute(1);
+    c.Undo();
+    c.Execute(10);
+    c.Execute(15);
+  }
+}
+}
+
