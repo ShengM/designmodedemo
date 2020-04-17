f53494233625274c88e099f91442f0cf6a8364d5
masheng <sheng7m@gmail.com>
2020-04-17 16:24:59 +0800

    备忘录模式

---
+++ 备忘录模式/Example.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+
+namespace designmodedemo.备忘录模式
+{
+  public class Chessman
+  {
+    public string Label { get; set; }
+
+    public int X { get; set; }
+
+    public int Y { get; set; }
+
+    public Chessman(string label, int x, int y)
+    {
+      this.Label = label;
+      this.X = x;
+      this.Y = y;
+    }
+
+    public ChessmanMemento Save()
+    {
+      return new ChessmanMemento(this.Label, this.X, this.Y);
+    }
+
+    public void Restore(ChessmanMemento m)
+    {
+      this.Label = m.Label;
+      this.X = m.X;
+      this.Y = m.Y;
+    }
+
+    public void Display()
+    {
+      Console.WriteLine($"棋子{this.Label}当前位置为：第{this.X}行第{this.Y}列");
+    }
+  }
+
+  public class ChessmanMemento
+  {
+    public string Label { get; set; }
+
+    public int X { get; set; }
+
+    public int Y { get; set; }
+
+    public ChessmanMemento(string label, int x, int y)
+    {
+      this.Label = label;
+      this.X = x;
+      this.Y = y;
+    }
+  }
+
+  public class MementoCaretaker
+  {
+    private const int MEMENTO_LENGTH = 5;
+    private ChessmanMemento[] _mementos = new ChessmanMemento[MEMENTO_LENGTH];
+    private int _caret = -1;
+
+    public void AddMemento(ChessmanMemento m)
+    {
+      _caret++;
+      if (_caret == MEMENTO_LENGTH)
+      {
+        _caret = 0;
+      }
+
+      _mementos[_caret] = m;
+    }
+
+    public ChessmanMemento Redo()
+    {
+      _caret++;
+      if (_caret == MEMENTO_LENGTH)
+      {
+        return null;
+      }
+
+      return _mementos[_caret];
+    }
+
+    public ChessmanMemento Undo()
+    {
+      _caret--;
+      if (_caret == -1)
+      {
+        return null;
+      }
+
+      return _mementos[_caret];
+    }
+  }
+}
---
+++ 备忘录模式/Starter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace designmodedemo.备忘录模式
+{
+  class Starter : IStart
+  {
+    public void Run()
+    {
+      var mc = new MementoCaretaker();
+      var c = new Chessman("车", 1, 1);
+      mc.AddMemento(c.Save());
+      c.Display();
+
+      c.X = 2;
+      mc.AddMemento(c.Save());
+      c.Display();
+
+      c.Y = 2;
+      mc.AddMemento(c.Save());
+      c.Display();
+
+      c.Restore(mc.Undo());
+      c.Display();
+
+      c.Restore(mc.Undo());
+      c.Display();
+
+      c.Restore(mc.Redo());
+      c.Display();
+
+      c.Restore(mc.Redo());
+      c.Display();
+    }
+  }
+}
