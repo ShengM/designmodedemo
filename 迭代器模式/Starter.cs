efe41867906fc4c77cf45e0680fcbd37432327c6
masheng <sheng7m@gmail.com>
2020-04-17 11:25:07 +0800

    添加迭代器模式示例

---
+++ 迭代器模式/Starter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace designmodedemo.迭代器模式
+{
+  class Starter : IStart
+  {
+    public void Run()
+    {
+      var l = new List<object>();
+      l.Add("小米");
+      l.Add("小明");
+      l.Add("小红");
+      var pl = new ProductList(l);
+      var i = pl.CreateIterator();
+
+      while (!i.IsLast())
+      {
+        Console.WriteLine(i.GetNextItem());
+        i.Next();
+      }
+
+      while (!i.IsFirst())
+      {
+        Console.WriteLine(i.GetPreviousItem());
+        i.Previous();
+      }
+    }
+  }
+}
---
+++ 迭代器模式/example1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace designmodedemo.迭代器模式
+{
+  public interface IIterator
+  {
+    void Previous();
+    void Next();
+    bool IsFirst();
+    bool IsLast();
+    object GetNextItem();
+    object GetPreviousItem();
+  }
+
+  public class ProductIterator : IIterator
+  {
+    private List<object> _productList;
+    private int _leftCursor;
+    private int _rightCursor;
+
+    public ProductIterator(ProductList productList)
+    {
+      _productList = productList.GetItems();
+      _leftCursor = 0;
+      _rightCursor =_productList.Count - 1;
+    }
+
+    public object GetNextItem()
+    {
+      return _productList[_leftCursor];
+    }
+
+    public object GetPreviousItem()
+    {
+      return _productList[_rightCursor];
+    }
+
+    public bool IsFirst()
+    {
+      return _rightCursor == -1;
+    }
+
+    public bool IsLast()
+    {
+      return _leftCursor == _productList.Count;
+    }
+
+    public void Next()
+    {
+      if (_leftCursor < _productList.Count)
+      {
+        _leftCursor++;
+      }
+    }
+
+    public void Previous()
+    {
+      if (_rightCursor > -1)
+      {
+        _rightCursor--;
+      }
+    }
+  }
+
+  public abstract class CollectionBase
+  {
+    protected List<object> _items = new List<object>();
+
+    public CollectionBase(List<object> items)
+    {
+      _items = items;
+    }
+
+    public void AddItem(object item)
+    {
+      _items.Add(item);
+    }
+
+    public void RemoveItem(object item)
+    {
+      _items.Remove(item);
+    }
+
+    public List<object> GetItems()
+    {
+      return _items;
+    }
+
+    public abstract IIterator CreateIterator();
+  }
+
+  public class ProductList : CollectionBase
+  {
+    public ProductList(List<object> products)
+      : base(products)
+    {
+
+    }
+
+    public override IIterator CreateIterator()
+    {
+      return new ProductIterator(this);
+    }
+  }
+}
