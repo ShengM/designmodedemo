cdeca5432d0b4a14c237fc655160cd9037e712c6
masheng <sheng7m@gmail.com>
2020-04-17 18:40:01 +0800

    添加观察者模式示例代码

---
+++ 观察者模式/Example.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace designmodedemo.观察者模式
+{
+  // 战场支援接口
+  public interface IAskForSupport
+  {
+    // 请求支援
+    void Ask(War war);
+
+    // 发起支援
+    void Support(Soldier soldier);
+  }
+
+  public class Soldier : IAskForSupport
+  {
+    public string Name { get; set; }
+
+    public Soldier(string name)
+    {
+      this.Name = name;
+    }
+
+    public void Ask(War war)
+    {
+      Console.WriteLine($"{this.Name}正在呼叫支援");
+      war.Notify(this);
+    }
+
+    public void Support(Soldier soldier)
+    {
+      Console.WriteLine($"{this.Name}正在前往支援{soldier.Name}");
+    }
+  }
+
+  public class War
+  {
+    private List<Soldier> _soldiers = new List<Soldier>();
+
+    public War Join(Soldier soldier)
+    {
+      _soldiers.Add(soldier);
+      return this;
+    }
+
+    public void Notify(Soldier sender)
+    {
+      _soldiers.ForEach(s =>
+      {
+        if (s != sender)
+        {
+          s.Support(sender);
+        }
+      });
+    }
+  }
+}
---
+++ 观察者模式/Starter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace designmodedemo.观察者模式
+{
+  public class Starter : IStart
+  {
+    public void Run()
+    {
+      var war = new War();
+      var s1 = new Soldier("许三多");
+      var s2 = new Soldier("吴哲");
+      var s3 = new Soldier("史今");
+      var s4 = new Soldier("伍六一");
+
+      war.Join(s1).Join(s2).Join(s3).Join(s4);
+
+      s1.Ask(war);
+    }
+  }
+}
